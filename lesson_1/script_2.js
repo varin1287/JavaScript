0ff5dd0473b147964d5327f86647a45ece2c9fb0
zvv1287 <zvv1287@mail.ru>
2020-10-31 14:13:59 +0300

    lesson_1

---
+++ lesson_1/script_2.js
@@ -0,0 +1,26 @@
+
+/* Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать
+значение из name в admin. Вывести admin (должно вывестись «Василий»).
+ */
+
+'use strict';
+alert('Задание 2')
+
+function copy_the_name(tem_C) {
+    return (9 / 5) * tem_C + 32;
+}
+
+let name = "Василий";
+let admin;
+admin = name;
+alert('admin =  ' + admin);
+
+
+
+
+
+
+
+
+
+
